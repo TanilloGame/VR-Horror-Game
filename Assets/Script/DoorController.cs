9f7c048442ee76ce723ebe8177e4044b98c1edbe
Lorenzo Herrero <lorentain2@gmail.com>
2025-03-19 19:25:19 +0100

    Puerta que necesita llave.

--- Assets/Script/DoorController.cs
+++ Assets/Script/DoorController.cs
@@ -6,16 +6,25 @@ public class DoorController : MonoBehaviour
 {
     [SerializeField] private Animator doorAnimator;
     [SerializeField] private GameObject door;
+    [SerializeField] private bool hasKey = false;
 
     public void DoorInteraction()
     {
-        if (doorAnimator.GetBool("Open"))
+        if (hasKey)
         {
-            doorAnimator.SetBool("Open", false);
+            if (doorAnimator.GetBool("Open"))
+            {
+                doorAnimator.SetBool("Open", false);
+            }
+            else
+            {
+                doorAnimator.SetBool("Open", true);
+            }
         }
-        else
-        {
-            doorAnimator.SetBool("Open", true);
-        }
+    }
+
+    public void SetKeyTrue()
+    {
+        hasKey = true;
     }
 }

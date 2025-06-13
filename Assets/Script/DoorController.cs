f2510ba05bedcffcaf0a5380a60c260a3b25e7c7
Lorenzo Herrero Salcedo <lorentain2@gmail.com>
2025-06-13 19:20:48 +0200

    Evento llave y puertas codigo nuevo.

--- Assets/Script/DoorController.cs
+++ Assets/Script/DoorController.cs
@@ -6,13 +6,14 @@ public class DoorController : MonoBehaviour
 {
     [SerializeField] private Animator doorAnimator;
     [SerializeField] private GameObject door;
-    [SerializeField] private bool hasKey = false;
+    [SerializeField] private bool needKey1 = false;
+    [SerializeField] private bool needKey2 = false;
     [SerializeField] private AudioSource doorClosingAudioSource;
     [SerializeField] private AudioSource doorOpeningAudioSource;
 
     public void DoorInteraction()
     {
-        if (hasKey)
+        if (needKey1 && PlayerInventory.HasKey1())
         {
             if (doorAnimator.GetBool("Open"))
             {
@@ -25,10 +26,31 @@ public class DoorController : MonoBehaviour
                 doorOpeningAudioSource.Play();
             }
         }
-    }
-
-    public void SetKeyTrue()
-    {
-        hasKey = true;
+        else if (needKey2 && PlayerInventory.HasKey2())
+        {
+            if (doorAnimator.GetBool("Open"))
+            {
+                doorAnimator.SetBool("Open", false);
+                doorClosingAudioSource.Play();
+            }
+            else
+            {
+                doorAnimator.SetBool("Open", true);
+                doorOpeningAudioSource.Play();
+            }
+        }
+        else
+        {
+            if (doorAnimator.GetBool("Open"))
+            {
+                doorAnimator.SetBool("Open", false);
+                doorClosingAudioSource.Play();
+            }
+            else
+            {
+                doorAnimator.SetBool("Open", true);
+                doorOpeningAudioSource.Play();
+            }
+        }
     }
 }
